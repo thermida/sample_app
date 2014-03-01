6053220db7896125becf0bba24332e39a024a7c1
Timothy Hermida <tim.hermida@gmail.com>
2014-03-01 21:09:42 +0000

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 SampleApp::Application.routes.draw do
-
-  get "users/new"
-  root to: 'static_pages#home'
-
-  get '/signup',    to: 'users#new'
-
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
